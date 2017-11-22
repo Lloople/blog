e96516b40697d791f83bb583d9d0ebd5dfc8ecd8
David Llop <d.lloople@gmail.com>
2017-11-22 23:23:48 +0100

    Change version number

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'version' => '0.5.0',
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
